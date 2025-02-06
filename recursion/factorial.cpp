4fe3a321db103c0cca8852e2f9067cac2a33ab1c
Rajveer Rai <157128843+Rajveer8169@users.noreply.github.com>
2025-02-06 19:54:38 +0530

    Update factorial.cpp

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -1,31 +1,9 @@
-//{ Driver Code Starts
-// Initial Template for C
-
-#include <stdio.h>
-
-
-// } Driver Code Ends
-// User function Template for C
-
 int factorial(int n) {
-    int fact = 1;
-    for (int i = 1; i <= n; ++i) {
-        fact *= i; 
+    if(n==1 || n==0){
+        return n;
     }
+    int result = factorial(n-1);
+    int fact = result * n;
     return fact;
-}
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    scanf("%d", &t);
-    while (t--) {
-        int N;
-        scanf("%d", &N);
-        printf("%lld\n", factorial(N));
-
-        printf("~%s", "\n");
-    }
-    return 0;
+    // code here
 }
