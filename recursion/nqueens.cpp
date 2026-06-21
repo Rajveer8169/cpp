ef6037c830d8628206c3964e3e7a6f64275efbbc
Rajveer Rai <157128843+Rajveer8169@users.noreply.github.com>
2026-06-21 22:45:55 +0530

    Refactor N-Queens solution to use strings for board

--- recursion/nqueens.cpp
+++ recursion/nqueens.cpp
@@ -1,72 +1,82 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-vector<vector<int>>ans;
+class Solution {
+public:
+    vector<vector<string>> ans;
 
-bool issafe(vector<vector<int>>& board , int r , int c , int n)
-{
-    for(int i = 0 ; i<r ; i++)
-    {
-        if(board[i][c]==1)
-        {
-            return false;
-        }
-    }
-    for(int i=r-1 , j=c-1 ; i>=0 && j>=0 ; i-- , j--)
-    {
-          if(board[i][j]==1)
-        {
-            return false;
-        }
-    }
-    for(int i=r-1 ,  j=c+1 ; i>=0 && j<n ; i-- , j++)
-    {
-          if(board[i][j]==1)
-        {
-            return false;
-        }
-    }
-    return true;
-}
+    // Backtracking function
+    // col -> current column where we need to place a queen
+    void helper(int col,
+                vector<int> &leftrow,
+                vector<int> &lowdiag,
+                vector<int> &updiag,
+                vector<string> &board,
+                int n) {
 
-void helper(vector<vector<int>>& board , int r , int n)
-{
-    if(r==n)
-    {
-        vector<int> temp;
-        for(int i=0 ; i<n ; i++)
-        {
-            for(int j=0 ; j<n ;j++)
-            {
-                if(board[i][j]==1)
-                {
-                    temp.push_back(j+1);
-                }
+        // Base case:
+        // If queens are placed in all columns,
+        // store the current board configuration.
+        if (col == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        // Try placing a queen in every row of the current column
+        for (int row = 0; row < n; row++) {
+
+            // Check if current position (row, col) is safe
+            // leftrow[row]           -> row occupied?
+            // lowdiag[row + col]     -> '\' diagonal occupied?
+            // updiag[n-1+col-row]    -> '/' diagonal occupied?
+            if (leftrow[row] == 0 &&
+                lowdiag[row + col] == 0 &&
+                updiag[n - 1 + col - row] == 0) {
+
+                // Place queen
+                board[row][col] = 'Q';
+
+                // Mark row and diagonals as occupied
+                leftrow[row] = 1;
+                lowdiag[row + col] = 1;
+                updiag[n - 1 + col - row] = 1;
+
+                // Recur for next column
+                helper(col + 1, leftrow, lowdiag, updiag, board, n);
+
+                // Backtrack:
+                // Remove queen and unmark row/diagonals
+                board[row][col] = '.';
+                leftrow[row] = 0;
+                lowdiag[row + col] = 0;
+                updiag[n - 1 + col - row] = 0;
             }
         }
-        ans.push_back(temp);
     }
-    for(int i=0 ; i<n ; i++)
-    {
-        if(issafe(board , r , i , n))
-        {
-            board[r][i]=1;
-            helper(board , r+1 , n);
-            board[r][i]=0;
-        }
-    }
-}
 
-int main ()
-{
-    int n=8;
-  vector<vector<int>> board(n, vector<int>(n, 0));
-    helper(board , 0 ,n);
-   for (auto& solution : ans) {
-        for (int pos : solution) {
-            cout << pos << " ";
+    vector<vector<string>> solveNQueens(int n) {
+
+        // Create an empty n x n chessboard filled with '.'
+        vector<string> board(n);
+        string s(n, '.');
+
+        for (int i = 0; i < n; i++) {
+            board[i] = s;
         }
-        cout << endl;
+
+        // Hash arrays for O(1) safety checking
+        vector<int> leftrow(n, 0);       // Tracks occupied rows
+        vector<int> lowdiag(2 * n - 1, 0); // Tracks '\' diagonals
+        vector<int> updiag(2 * n - 1, 0);  // Tracks '/' diagonals
+
+        // Start placing queens from column 0
+        helper(0, leftrow, lowdiag, updiag, board, n);
+
+        return ans;
     }
-    return 0;
-}
+};
+
+TC = O(N!)
+
+Space Complexity
+Board storage
+O(N
+2
+)
